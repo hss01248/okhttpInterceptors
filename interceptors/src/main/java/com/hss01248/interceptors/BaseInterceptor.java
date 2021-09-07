06502e097d9818d0d3d47d1c00ea8fdbd4e95d95
hss01248 <hss012489@gmail.com>
2021-09-07 14:12:31 +0800

    优化api设计

--- interceptors/src/main/java/com/hss01248/interceptors/BaseInterceptor.java
+++ interceptors/src/main/java/com/hss01248/interceptors/BaseInterceptor.java
@@ -9,7 +9,7 @@ import java.util.Set;
 import okhttp3.Interceptor;
 import okhttp3.Response;
 
-/**
+/** * shouldIntercept方法优先级最高,然后才是白名单,黑名单
  * 白名单,黑名单
  * <p>
  * 优先白名单,只有在白名单里的,才拦截
@@ -25,28 +25,43 @@ public abstract class BaseInterceptor implements Interceptor {
     protected Set<String> whiteListUrlPattern;
     protected Set<String> blackListUrlPattern;
 
-    public void setDebug(boolean debug) {
+    public BaseInterceptor setDebug(boolean debug) {
         isDebug = debug;
+        return this;
     }
 
     protected boolean isDebug;
+    protected boolean interceptorAll;
+    public BaseInterceptor interceptorAll(boolean interceptorAll){
+        this.interceptorAll = interceptorAll;
+        return this;
+    }
 
-    public void addWhiteList(String pattern) {
+    public BaseInterceptor addWhiteList(String pattern) {
         if (whiteListUrlPattern == null) {
             whiteListUrlPattern = new HashSet<>();
         }
         whiteListUrlPattern.add(pattern);
+        return this;
     }
 
-    public void addBlackList(String pattern) {
+    public BaseInterceptor addBlackList(String pattern) {
         if (blackListUrlPattern == null) {
             blackListUrlPattern = new HashSet<>();
         }
         blackListUrlPattern.add(pattern);
+        return this;
     }
 
     @Override
     public Response intercept(Chain chain) throws IOException {
+        if(interceptorAll){
+            return interceptReally(chain);
+        }
+
+        if(shouldInterceptBeforeUrlWhiteBlackList(chain)){
+            return interceptReally(chain);
+        }
         String url = chain.request().url().toString();
 
         if (whiteListUrlPattern != null && !whiteListUrlPattern.isEmpty()) {
@@ -58,10 +73,7 @@ public abstract class BaseInterceptor implements Interceptor {
             }
             //有白名单,但不在白名单里,就不拦截
             return chain.proceed(chain.request());
-        }
-
-
-        if (blackListUrlPattern != null && !blackListUrlPattern.isEmpty()) {
+        }else if (blackListUrlPattern != null && !blackListUrlPattern.isEmpty()) {
             //有黑名单,在黑名单里的,就不拦截
             for (String pattern : blackListUrlPattern) {
                 if (url.contains(pattern)) {
@@ -73,6 +85,10 @@ public abstract class BaseInterceptor implements Interceptor {
         }
         return chain.proceed(chain.request());
 
+    }
+
+    protected boolean shouldInterceptBeforeUrlWhiteBlackList(Chain chain) {
+        return false;
     }
 
     protected abstract Response interceptReally(Chain chain) throws IOException;
